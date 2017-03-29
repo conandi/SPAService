9164fe994ea098fdb2149f83f7f5a83ce7ad900a
Conan Di <conandi27@gmail.com>
2017-03-29 09:35:35 -0700

    commit test

--- WellsFargoProject/WellsFargoProject/ViewController.swift
+++ WellsFargoProject/WellsFargoProject/ViewController.swift
@@ -20,8 +20,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         prepareData()
         prepareView()
+        print("Test for new branch!!")
+
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
